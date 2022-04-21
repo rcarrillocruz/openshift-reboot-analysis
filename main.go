7e8bccd5d824a4599218ce03923b6d425521c1f5
Ricardo Carrillo Cruz <ricarril@redhat.com>
2022-04-21 11:45:27 +0200

    Untar file from flag

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"archive/tar"
 	"flag"
 	"fmt"
+	"log"
+	"os"
 )
 
 var mg = flag.String("must-gather", "must-gather.tar", "The path to the must-father.tar file to analyze")
@@ -14,4 +17,19 @@ func init() {
 func main() {
 	flag.Parse()
 	fmt.Printf("Must-gather: %s\n", *mg)
+
+	f, err := os.Open(*mg)
+	if err != nil {
+		log.Fatal("Could not open file")
+	}
+
+	tr := tar.NewReader(f)
+	h, err := tr.Next()
+	if err != nil {
+		log.Fatalf("Could not untar file: %s", err)
+	}
+
+	fmt.Printf("Tar reader: %v", tr)
+	fmt.Printf("Tar header: %v", h)
+
 }
